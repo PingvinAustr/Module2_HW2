f2ea23520b2ea53dd9141de82cc6ab4d0858985a
Yaroslav <yarik.chushenko1@gmail.com>
2022-10-30 16:00:07 +0200

    Initial directory changes

--- NIX_Module2_HW2/NIX_Module2_HW2/Program.cs
+++ NIX_Module2_HW2/NIX_Module2_HW2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace NIX_Module2_HW2
+{
+    internal class Program
+    {
+        internal void Main(string[] args)
+        {
+            // Hi
+        }
+    }
+}
